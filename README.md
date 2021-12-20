797c6a258c2b1ec01e287a58b1d16d048a31b73f
rupansh <rupanshsekar@hotmail.com>
2021-12-20 08:29:19 +0530

    add docs

---
+++ README.md
@@ -0,0 +1,16 @@
+# Whales-Swap
+
+## Compile
+
+`pnpm run build`
+
+Markdown docs are automatically generated in the `./docs` folder  
+Typings are auto generated in `./typechain-types`
+
+## Deploy
+
+setup `.env`
+
+`pnpx hardhat run scripts/<script_name>.ts --network <network>`
+
+see hardhat.config.ts for networks
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,17 @@
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import '@typechain/hardhat';
+import '@primitivefi/hardhat-dodoc';
+import { config } from 'dotenv';
+config();
+
+const accounts = process.env.SIGNER ? [process.env.SIGNER] : [];
 
 export default {
   solidity: "0.8.7",
+  docdoc: {
+    runOnCompile: true
+  },
   networks: {
     hardhat: {
       mining: {
@@ -13,7 +21,11 @@ export default {
     },
     bsc_testnet: {
       url: "https://data-seed-prebsc-1-s2.binance.org:8545",
-      accounts: [process.env.SIGNER],
+      accounts,
+    },
+    bsc_mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      accounts
     }
   },
 };
